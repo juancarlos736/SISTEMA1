5e23562a9f8e7c7bf8b79bf7e05fca8046184a33
Juan Carlos Romero <59720743+juancarlos736@users.noreply.github.com>
2024-07-21 03:34:56 -0500

    commit2
    cambio_variables_de_entorno

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose")
 
-const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env
-const MONGODB_URI=`mongodb://127.0.0.1:27017/note-app`
+const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_URI} = process.env
+
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
