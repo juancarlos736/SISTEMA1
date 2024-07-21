698580f99461f7b3f1cd8d5703ed23d3168aa89b
Juan Carlos Romero <59720743+juancarlos736@users.noreply.github.com>
2024-07-21 03:44:17 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose")
 
 const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_URI} = process.env
-
+const MONGODB_URI=`mongodb+srv://<admin1289>:<adhd8493S89>@cluster0.yn2xhtr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
